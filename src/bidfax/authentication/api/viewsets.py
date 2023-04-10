0ef6d1d0d4d255448245dc3babefccfa53f8cad1
Ilya Verbilo <verbiloilyaa@gmail.com>
2023-04-10 21:15:29 +0300

    config for Postgresql

--- src/bidfax/authentication/api/viewsets.py
+++ src/bidfax/authentication/api/viewsets.py
@@ -2,8 +2,7 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import viewsets
 
-from bidfax.authentication.models import Profile
-from bidfax.authentication.api.serializers import UserSerializer, ProfileSerializer
+from bidfax.authentication.api.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
