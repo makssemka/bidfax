073e20000c034fb868264d5be78bde5b7b61ff95
Ilya Verbilo <verbiloilyaa@gmail.com>
2023-04-12 16:52:32 +0300

    edit creation user via admin

--- src/bidfax/authentication/admin.py
+++ src/bidfax/authentication/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
-from bidfax.authentication.models import Profile
+from bidfax.authentication.models import User, Profile
+from bidfax.authentication.forms import UserCreationForm
 
-admin.site.register(get_user_model())
+
+class UserAdmin(DefaultUserAdmin):
+    add_form = UserCreationForm
+    model = User
+    list_display = ('email', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'uid')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'password1', 'password2', 'is_staff',
+                'is_active', 'groups', 'user_permissions'
+            )
+        }
+        ),
+    )
+    readonly_fields = ('uid',)
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Profile)
