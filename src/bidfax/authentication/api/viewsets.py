e644a32c4138b898f6bc82bf0cd9d28ddbf4dc89
elilvina <hi.elilvina@gmail.com>
2023-04-15 15:43:55 +0300

    edit viewsets.py

--- src/bidfax/authentication/api/viewsets.py
+++ src/bidfax/authentication/api/viewsets.py
@@ -1,10 +1,12 @@
-from django.contrib.auth import get_user_model
-
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework import viewsets
 
+from bidfax.authentication.models import User
 from bidfax.authentication.api.serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = get_user_model().objects.all()
+class UserViewSet(viewsets.ModelViewSet, ):
+    lookup_field = 'uid'
+    queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
