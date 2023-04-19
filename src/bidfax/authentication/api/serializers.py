23f819d559a6f02bfe5dcb6031a6f4ff8d831fd6
Ilya Verbilo <verbiloilyaa@gmail.com>
2023-04-19 17:04:01 +0300

    add token serializer

--- src/bidfax/authentication/api/serializers.py
+++ src/bidfax/authentication/api/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate
+
 from rest_framework import serializers
 
 from bidfax.authentication.models import User, Profile
@@ -29,3 +31,24 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('first_name', 'last_name', 'avatar', 'user')
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField(label='Email', write_only=True)
+    password = serializers.CharField(label='Password', trim_whitespace=True, write_only=True)
+    token = serializers.CharField(label='Token', read_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = authenticate(request=self.context.get('request'), email=email, password=password)
+            if not user:
+                raise serializers.ValidationError('Unable to log in with provided credentials', code='authorization')
+
+        else:
+            raise serializers.ValidationError('Must include "email" and "password".', code='authorization')
+
+        attrs['user'] = user
+        return user
