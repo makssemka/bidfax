b363828fea85a80f61f386eb4d3b352580ecd142
Ilya Verbilo <verbiloilyaa@gmail.com>
2023-04-19 17:50:47 +0300

    change view for token endpoint

--- src/bidfax/authentication/urls.py
+++ src/bidfax/authentication/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
 
 from rest_framework import routers
-from rest_framework.authtoken.views import obtain_auth_token
 
 from bidfax.authentication.api.viewsets import UserViewSet
-from bidfax.authentication.api.views import ProfileView
+from bidfax.authentication.api.views import ProfileView, TokenView
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path(route='profile/<int:pk>', view=ProfileView.as_view(), name='profiles'),
-    path(route='token/login', view=obtain_auth_token, name='login')
+    path(route='token/login', view=TokenView.as_view(), name='login')
 ]
 
 urlpatterns += router.urls
