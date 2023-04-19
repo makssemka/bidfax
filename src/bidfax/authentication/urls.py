fd19885c031f68fc3f861ca035eba05703c8b7f3
Ilya Verbilo <verbiloilyaa@gmail.com>
2023-04-19 15:36:11 +0300

    add obtain token endpoint

--- src/bidfax/authentication/urls.py
+++ src/bidfax/authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 from bidfax.authentication.api.viewsets import UserViewSet
 from bidfax.authentication.api.views import ProfileView
@@ -10,6 +11,7 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path(route='profile/<int:pk>', view=ProfileView.as_view(), name='profiles'),
+    path(route='token/login', view=obtain_auth_token, name='login')
 ]
 
 urlpatterns += router.urls
