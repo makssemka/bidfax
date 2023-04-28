d5c8ef1756e7694164910f5b505fb9d3199f7fca
makssemka <makssemka23@gmail.com>
2023-04-28 20:20:08 +0300

    add models

--- src/bidfax/auction/admin.py
+++ src/bidfax/auction/admin.py
@@ -1,3 +1,14 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from bidfax.auction.models import Brand, Model, Condition, Spec, Information, Auction, Lot
+
+
+admin.site.register(Brand)
+admin.site.register(Model)
+admin.site.register(Condition)
+admin.site.register(Spec)
+admin.site.register(Information)
+admin.site.register(Auction)
+admin.site.register(Lot)
 
 # Register your models here.
--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'drf_yasg',
 
     'bidfax.authentication',
+    'bidfax.auction',
 ]
 
 MIDDLEWARE = [
