caac9b9bce60edc75bf2b4a75e3fea0c9aed42b8
Ilya Verbilo <verbiloilyaa@gmail.com>
2023-05-03 22:23:12 +0300

    refactor save car data service

--- src/bidfax/auction/services/db.py
+++ src/bidfax/auction/services/db.py
@@ -1,8 +1,8 @@
-from bidfax.auction.services.scraper import get_models_names
+from bidfax.auction.services.scraper import get_car_models_data
 from bidfax.auction.models import Brand, Model
 
 
-def car_save():
-    for car in get_models_names():
+def car_models_save():
+    for car in get_car_models_data():
         brand, created = Brand.objects.get_or_create(name=car['name'])
         Model.objects.bulk_create([Model(name=model, brand=brand) for model in car['models']])
