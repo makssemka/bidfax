bba6f28445f316ec9bcc025bc439cb60bdb5c6b7
Ilya Verbilo <verbiloilyaa@gmail.com>
2023-05-10 14:33:06 +0300

    edit saving car models data

--- src/bidfax/auction/services/db.py
+++ src/bidfax/auction/services/db.py
@@ -1,8 +1,8 @@
 from bidfax.auction.services.scraper import get_car_models_data
-from bidfax.auction.models import Brand, Model
+from bidfax.auction.models import Brand, CarModel
 
 
 def car_models_save():
     for car in get_car_models_data():
         brand, created = Brand.objects.get_or_create(name=car['name'])
-        Model.objects.bulk_create([Model(name=model, brand=brand) for model in car['models']])
+        CarModel.objects.bulk_create([CarModel(name=model, brand=brand) for model in car['models']])
