a4b055109e75abb40f5d08b412997e12dc0d8e51
Ilya Verbilo <verbiloilyaa@gmail.com>
2023-05-11 15:54:13 +0300

    fix auction models: add str methods

--- src/bidfax/auction/models.py
+++ src/bidfax/auction/models.py
@@ -47,13 +47,8 @@ class Spec(models.Model):
     engine = models.CharField(max_length=100, verbose_name='Двигатель')
     fuel = models.CharField(max_length=100, verbose_name='Тип топлива')
 
-    # class Meta:
-    #     constraints = [
-    #         models.UniqueConstraint(
-    #             fields=['transmission', 'drive', 'fuel'],
-    #             name='unique_spec'
-    #         )
-    #     ]
+    def __str__(self):
+        return self.engine
 
 
 class Information(models.Model):
@@ -62,12 +57,18 @@ class Information(models.Model):
     repair_price = models.CharField(max_length=100, verbose_name='Стоимость ремонта')
     note = models.CharField(max_length=100, verbose_name='Примечание')
 
+    def __str__(self):
+        return self.lot_number
+
 
 class Auction(models.Model):
     auction_name = models.CharField(max_length=100, verbose_name='Имя аукциона')
     documents = models.CharField(max_length=100, verbose_name='Документы')
     location = models.CharField(max_length=100, verbose_name='Локация')
     seller = models.CharField(max_length=100, verbose_name='Продавец')
+
+    def __str__(self):
+        return self.auction_name
 
 
 class Lot(models.Model):
@@ -83,3 +84,6 @@ class Lot(models.Model):
     information = models.ForeignKey(Information, on_delete=models.CASCADE)
     auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
     car_model = models.ForeignKey(CarModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.car_model.name} {self.car_model.brand}'
