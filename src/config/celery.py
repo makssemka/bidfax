406ec323c5b4168da24d8801885525ebbadfaa84
Ilya Verbilo <verbiloilyaa@gmail.com>
2023-05-18 15:20:21 +0300

    refactor celery configs

---
+++ src/config/celery.py
@@ -0,0 +1,12 @@
+import os
+
+from django.conf import settings
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
+
+app = Celery('config')
+app.config_from_object(settings, namespace='CELERY')
+app.conf.broker_url = settings.CELERY_BROKER_URL
+app.autodiscover_tasks()
